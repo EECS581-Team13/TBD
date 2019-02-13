072db57f5871465666c521853e711d425d2d8ba4
Bud Linville <budlinville@gmail.com>
2019-02-13 13:01:33 -0600

    Added C++ files for generating character stats.

---
+++ Source/STRIKERR/CharGen/CharGen.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CharGen.h"
+
+CharGen::CharGen() {
+	setHeight();
+	setStrength();
+	setGrit();
+	setEndurance();
+	setAccuracy();
+	setCoord();
+	setAge();
+	setName();
+	setDesc();
+}
+
+CharGen::~CharGen() {
+
+}
+
+void CharGen::setHeight() {
+
+}
+
+void CharGen::setStrength() {
+
+}
+
+void CharGen::setGrit() {
+
+}
+
+void CharGen::setEndurance() {
+
+}
+
+void CharGen::setAccuracy() {
+}
+
+void CharGen::setCoord() {
+}
+
+void CharGen::setAge() {
+}
+
+void CharGen::setName() {
+}
+
+void CharGen::setDesc() {
+}
+
+int CharGen::getHeight() {
+}
+
+int CharGen::getStength() {
+}
+
+int CharGen::getGrit() {
+}
+
+int CharGen::getEndurance() {
+}
+
+int CharGen::getAccuracy() {
+}
+
+int CharGen::getCoord() {
+}
+
+int CharGen::getAge() {
+}
+
+std::string CharGen::getName() {
+}
+
+std::string CharGen::getDesc() {
+}
---
+++ Source/STRIKERR/CharGen/CharGen.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class STRIKERR_API CharGen {
+private:
+	//attributes that affect gameplay
+	int height;			//(inches) Resistance to being hit by other players
+	int strength;		// How far you can knock the ball
+	int grit;			// How far you can knock other players
+	int endurance;		// How far a player can travel
+	int accuracy;		// How straight you can kick a ball
+	int coord;			// Coordination : Resistance to injury
+	int age;			// Time until retirement
+	// int powerup;		//TODO: implement later
+
+	//cosmetic attributes
+	std::string name;
+	std::string desc;	// Player description
+
+	void setHeight();
+	void setStrength();
+	void setGrit();
+	void setEndurance();
+	void setAccuracy();
+	void setCoord();
+	void setAge();
+
+	void setName();
+	void setDesc();
+
+public:
+	CharGen();
+	~CharGen();
+
+	int getHeight();
+	int getStength();
+	int getGrit();
+	int getEndurance();
+	int getAccuracy();
+	int getCoord();
+	int getAge();
+
+	std::string getName();
+	std::string getDesc();
+};
